1ae3f1e8beabb94d0e12cf07267592f4eb43c62b
rissole <bonzaibrother@gmail.com>
2013-11-13 19:05:37 +1100

    some member stats

--- app.py
+++ app.py
@@ -1,20 +1,112 @@
 import os
+import facebook
+import json
+import re
 from flask import Flask
+from member import Member
 from apscheduler.scheduler import Scheduler
+
+### APP SETUP ###
 
 # Start the scheduler
 sched = Scheduler()
 sched.start()
 
+# Prepare Flask
 app = Flask(__name__)
+
+# Prepare Facebook
+oauth = ''
+with open('oauth') as f:
+	oauth = f.read()
+GRAPH = facebook.GraphAPI(oauth)
+
+## should we do the slow process of getting who likes each comment?
+GET_LIKERS = False
+
+## Group IDs
+GROUPS = {'320':'155001391342630', '420':'400112543380892'}
+
+#################
+
+def downloadGroupJSON(groupID):
+    open(groupID+'_feed.json', 'w').close()
+    kwargs = {
+        'limit': 1000,
+        'fields': 'comments.limit(500),created_time,from,full_picture,description,icon,id,link,message,message_tags,name,object_id,picture,caption,source,status_type,type,updated_time,with_tags,likes,actions'
+    }
+    total_feed = {'data': []}
+    while True:
+        feed = GRAPH.get_object(groupID+'/feed', **kwargs)
+        if feed['data']:
+            total_feed['data'] += feed['data']
+            until = re.search(r'until=(\d+)', feed['paging']['next']).group(1)
+            print "until:", until
+            kwargs['until'] = until
+            kwargs['offset'] = 1
+        else:
+            break
+    with open(groupID+'_feed.json', 'a') as f:
+        f.write(json.dumps(total_feed))
+        
+def get_likers_for_comment(post_id, comment_id):
+	if GET_LIKERS == False:
+		return []
+	else:
+		print('getting liker data')
+		likers_data = GRAPH.get_object(post_id+'_'+comment_id+'/likes', fields='name')
+		likers = [ l['name'] for l in likers_data['data'] ]
+		return likers
+
+def get_lastcheck():
+    # "a+" => open file for reading and writing, does not truncate
+    f = open('lastcheck', 'a+')
+    lastcheck = f.read()
+    f.close()
+    if (lastcheck != ''):
+        lastcheck = float(lastcheck)
+    print 'Checking... lastcheck=' + str(lastcheck)
+    return lastcheck
 
 @app.route('/')
 def root():
     return 'Hello World!'
 
 if __name__ == '__main__':
+    with open(GROUPS['320']+'_feed.json') as f:
+        jjson = f.read()
+    d = json.loads(jjson)
+    for post in d['data']:
+        poster = Member.make_or_get_member(post['from']['name'])
+        poster.add_post(post)
+        
+        if 'comments' in post:
+            comments = post['comments']['data']
+            for comment in comments:
+                commenter = Member.make_or_get_member(comment['from']['name'])
+                commenter.add_comment(comment)
+                
+        if 'likes' in post:
+            likes = post['likes']['data']
+            for like in likes:
+                liker = Member.make_or_get_member(like['name'])
+                liker.add_liked_post(post)
+                
+    for member in Member.members.values():
+        member.calc_num_posts()
+        member.calc_num_comments()
+        member.calc_num_liked_posts()
+        member.calc_post_likes_received()
+        member.calc_comment_likes_received()
+        member.calc_who_liked_posts()
+        member.calc_num_irrelevant_posts()
+        member.calc_average_comment_length()
+        member.calc_longest_comment()
+        member.print_stats()
+        print ''
+    
     #sched.add_interval_job(poo, seconds=1)
     
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)#, use_reloader=False)
+    #port = int(os.environ.get('PORT', 5000))
+    #app.run(host='0.0.0.0', port=port, debug=True)#, use_reloader=False)
---
+++ member.py
@@ -0,0 +1,179 @@
+class Member:
+    # {name -> class obj} of existing members
+    members = {}
+    
+    # stat enum, keys of this dict are valid keys for _stats
+    stats = {
+        'NUM_POSTS': 'Posts',
+        'NUM_COMMENTS': 'Comments',
+        'NUM_LIKED_POSTS': 'Posts likes given',
+        'POST_LIKES_RECEIVED': 'Post likes received',
+        'COMMENT_LIKES_RECEIVED': 'Comment likes received',
+        'WHO_LIKED_MY_POSTS': 'Users who liked my posts',
+        'IRRELEVANT_POSTS': 'Irrelevant posts',
+        'AVERAGE_COMMENT_LENGTH': 'Average comment length (chars)',
+        'LONGEST_COMMENT': 'Longest comment',
+    }
+    
+    @classmethod
+    def make_or_get_member(self, name):
+        if name in Member.members:
+            return Member.members[name]
+        else:
+            return Member(name)
+    
+    def __init__(self, name):
+        self._name = name
+        self._posts = []
+        self._comments = []
+        self._liked_posts = []
+        self._stats = {}
+        Member.members[name] = self
+        
+    def add_post(self, p):
+        self._posts.append(p)
+    
+    def add_comment(self, c):
+        self._comments.append(c)
+        
+    def add_liked_post(self, p):
+        self._liked_posts.append(p)
+        
+    def get_name(self):
+        return self._name
+        
+    def get_posts(self):
+        return self._posts
+    
+    def get_comments(self):
+        return self._comments
+        
+    def get_liked_posts(self):
+        return self._liked_posts
+        
+    def print_stats(self):
+        print self.get_name() + ':'
+        for stat_key, stat in Member.stats.items():
+            print stat + ':', self.get_stat(stat_key)
+    
+    # returns None if stat not set
+    def get_stat(self, stat_key):
+        return self._stats.get(stat_key)
+        
+    # forces the stat to be recalculated next time it's requested
+    def purge_stat(self, stat_key):
+        self._stats.pop(stat_key, None)
+    
+    # STATS CALCS #
+    # NUM_POSTS
+    def calc_num_posts(self):
+        cached = self.get_stat('NUM_POSTS')
+        if cached != None:
+            return cached
+        
+        self._stats['NUM_POSTS'] = len(self._posts)
+        return self._stats['NUM_POSTS']
+        
+    # NUM_COMMENTS
+    def calc_num_comments(self):
+        cached = self.get_stat('NUM_COMMENTS')
+        if cached != None:
+            return cached
+        
+        self._stats['NUM_COMMENTS'] = len(self._comments)
+        return self._stats['NUM_COMMENTS']
+        
+    # NUM_LIKED_POSTS
+    def calc_num_liked_posts(self):
+        cached = self.get_stat('NUM_LIKED_POSTS')
+        if cached != None:
+            return cached
+        
+        self._stats['NUM_LIKED_POSTS'] = len(self._liked_posts)
+        return self._stats['NUM_LIKED_POSTS']
+    
+    # POST_LIKES_RECEIVED
+    def calc_post_likes_received(self):
+        cached = self.get_stat('POST_LIKES_RECEIVED')
+        if cached != None:
+            return cached
+        
+        likes = 0
+        for post in self._posts:
+            if 'likes' in post:
+                likes += len(post['likes']['data'])
+        
+        self._stats['POST_LIKES_RECEIVED'] = likes
+        return self._stats['POST_LIKES_RECEIVED']
+        
+    # COMMENT_LIKES_RECEIVED
+    def calc_comment_likes_received(self):
+        cached = self.get_stat('COMMENT_LIKES_RECEIVED')
+        if cached != None:
+            return cached
+        
+        likes = 0
+        for comment in self._comments:
+            likes += comment['like_count']
+        
+        self._stats['COMMENT_LIKES_RECEIVED'] = likes
+        return self._stats['COMMENT_LIKES_RECEIVED']
+        
+    # WHO_LIKED_MY_POSTS
+    def calc_who_liked_posts(self):
+        cached = self.get_stat('WHO_LIKED_MY_POSTS')
+        if cached != None:
+            return cached
+        
+        likes = {}
+        for name in Member.members.keys():
+            likes[name] = 0
+            
+        for post in self._posts:
+            if 'likes' in post:
+                for like in post['likes']['data']:
+                    likes[like['name']] += 1
+        
+        self._stats['WHO_LIKED_MY_POSTS'] = likes
+        return self._stats['WHO_LIKED_MY_POSTS']
+        
+    # IRRELEVANT_POSTS
+    def calc_num_irrelevant_posts(self):
+        cached = self.get_stat('IRRELEVANT_POSTS')
+        if cached != None:
+            return cached
+        
+        posts = []
+        for post in self._posts:
+            if 'likes' in post:
+                continue
+            if 'comments' in post:
+                continue
+            posts.append(post['id'])
+        
+        self._stats['IRRELEVANT_POSTS'] = posts
+        return self._stats['IRRELEVANT_POSTS']
+        
+    # AVERAGE_COMMENT_LENGTH
+    def calc_average_comment_length(self):
+        cached = self.get_stat('AVERAGE_COMMENT_LENGTH')
+        if cached != None:
+            return cached
+        
+        total_length = 0
+        for comment in self._comments:
+            total_length += len(comment['message'])
+        
+        self._stats['AVERAGE_COMMENT_LENGTH'] = total_length / self.get_stat('NUM_COMMENTS')
+        return self._stats['AVERAGE_COMMENT_LENGTH']
+        
+    # LONGEST_COMMENT
+    def calc_longest_comment(self):
+        cached = self.get_stat('LONGEST_COMMENT')
+        if cached != None:
+            return cached
+        
+        longest_comment = max(self._comments, key=lambda x: len(x['message']))
+        
+        self._stats['LONGEST_COMMENT'] = {'id': longest_comment['id'], 'message': longest_comment['message']}
+        return self._stats['LONGEST_COMMENT']
